85a3361cf519c4f9e994e3b338ad047bcd9723a9
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-25 15:22:04 -0600

    Completed select_exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -12,6 +12,14 @@ FROM albums WHERE name = 'Nevermind';
 SELECT name AS 'These albums were released in the 1990''s:'
 FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 
+SELECT name AS 'These albums had less than 20 million certified sales:'
+FROM albums WHERE sales < 20;
+
+SELECT name AS 'These albums are in the ''Rock'' genre'
+FROM albums WHERE genre = 'Rock';
+
+
+
 
 
 
