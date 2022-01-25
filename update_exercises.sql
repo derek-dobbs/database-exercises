5e2a88a71e6517b203829d15b7e01a4bf28f1492
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-25 17:09:49 -0600

    Updated exercise, but need to fix code

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,10 +1,26 @@
 USE codeup_test_db;
 
-SELECT name AS 'Here are all the albums:'
-FROM albums;
+-- The code on the next two lines doesn't work:
+-- SELECT * AS 'Here are all the albums:'
+-- FROM albums;
+--------------------------------------------
+SELECT * FROM albums;
 
+UPDATE albums
+SET sales = (sales * 10);
+
+SELECT * FROM albums;
+--------------------------------------------
 SELECT name AS 'Here are all albums released before 1980:'
 FROM albums WHERE release_date < 1980;
+
+UPDATE albums
+SET release_date = (release_date - 100);
+
+-- SELECT name AS 'Here are all albums released before 1980 converted to the 1800s:'
+-- FROM albums WHERE release_date < 1880;
+
+--------------------------------------------
 
 SELECT name AS 'Here are the albums by Michael Jackson:'
 FROM albums WHERE artist = 'Michael Jackson';
