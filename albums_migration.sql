4d131c51cb08ae0c82d37b3f8f9903ea62bd5adf
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-25 09:40:27 -0600

    Completed exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,16 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+
 USE codeup_test_db;
 
+-- //Write some SQL to drop a table named albums if it exists.
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    name VARCHAR(50),
+    release_date CHAR(4),
+    sales FLOAT UNSIGNED,
+    genre VARCHAR(50),
+    PRIMARY KEY (id)
+);
