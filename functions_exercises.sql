77f64f61e6925f1ddca1b70406ef7533cfa53110
Derek Dobbs <91964686+derek-dobbs@users.noreply.github.com>
2022-01-27 12:00:16 -0600

    Completed Number Two of exercise

---
+++ functions_exercises.sql
@@ -0,0 +1,24 @@
+USE employees;
+
+# SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') AND gender IN ('M') ORDER BY first_name;
+SELECT * FROM employees WHERE (first_name IN ('Irena')
+    OR first_name IN ('Vidya')
+    OR first_name IN('Maya'))
+                          AND gender IN ('M')
+ORDER BY last_name DESC, first_name DESC;
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+# better way to do it: SELECT * FROM employees WHERE last_name LIKE 'E%E';
+SELECT * FROM employees WHERE (last_name LIKE 'E%') OR (last_name LIKE '%E');
+
+# Update your queries for employees whose names start and end with 'E'. Use concat() to combine their first and last name together as a single column in your results.
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE (last_name LIKE 'E%') OR (last_name LIKE '%E');
+
+SELECT * FROM employees WHERE (last_name LIKE 'E%') OR (last_name LIKE '%E');
+
+SELECT * FROM employees WHERE (last_name LIKE 'E%') AND (last_name LIKE '%E');
+
+SELECT * FROM employees WHERE (last_name LIKE ('%q%')) AND (last_name NOT LIKE ('%qu%'));
